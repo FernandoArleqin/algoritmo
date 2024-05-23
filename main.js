7568da51f069a92118fb96ebe5908082fb3ffaab
Fernando <fm3509118@gmail.com>
2024-05-23 11:58:56 -0300

    javascriptaula

---
+++ main.js
@@ -0,0 +1,100 @@
+// Aqui eu tenho um comentario
+
+console.log("Olá mundo")
+console.log("  ")
+console.log("______________________________________1")
+console.log("  ")
+
+// String
+// Int
+// Float - Double
+// Boolean
+
+// Guarde os dados do seu endereço em Variaveis (ex. rua, bairro, cep, estado...)
+// Guarde os seus dados (nome, email, telefone, data de nacimento) em Variaveis
+// Crie uma Variavel que diz se voce e maior de idade
+// Crie uma Variavel que diz se voce pode dirigir
+
+var rua = "Rua das Azaleias"
+var bairro = "Cidade Jardim II"
+var cep = "13467-120"
+var estado = "São Paulo"
+console.log("Rua: ", rua)
+console.log("Bairro; ", bairro)
+console.log("CEP: ", cep)
+console.log("Estado: ", estado)
+console.log("  ")
+console.log("______________________________________2")
+console.log("  ")
+
+var nome = "Fernando Macedo de Melo Júnior"
+var email = "fm3509118@gmail.com"
+var telefone = "+55 19 98902-6090"
+var dataNascimento = "12/02/2006"
+var genero = "Masculino"
+console.log("Nome: ", nome)
+console.log("Email: ", email)
+console.log("Número de contato: ", telefone)
+console.log("Data de nascimento: ", dataNascimento)
+console.log("Gênero: ", genero)
+console.log("  ")
+console.log("______________________________________3")
+console.log("  ")
+
+var maiorIdade = true
+var podeDirigir = false
+console.log("Maior idade: ", maiorIdade)
+console.log("Pode dirigir: ", podeDirigir)
+console.log("  ")
+console.log("______________________________________4")
+console.log("  ")
+
+var zero = 0
+var um = 1
+var dois = 2 
+var tres = 3
+var quatro = 4
+var cinco = 5
+var seis = 6
+var sete = 7
+var oito = 8
+var nove = 9
+var dez = 10
+
+var mtp = 9
+
+
+
+console.log(mtp, "x 0 =", zero*mtp)
+console.log(mtp, "x 1 =", um*mtp)
+console.log(mtp, "x 2 =", dois*mtp)
+console.log(mtp, "x 3 =", tres*mtp)
+console.log(mtp, "x 4 =", quatro*mtp)
+console.log(mtp, "x 5 =", cinco*mtp)
+console.log(mtp, "x 6 =", seis*mtp)
+console.log(mtp, "x 7 =", sete*mtp)
+console.log(mtp, "x 8 =", oito*mtp)
+console.log(mtp, "x 9 =", nove*mtp)
+console.log(mtp, "x 10 =", dez*mtp)
+
+//Maior 10 > 9 true
+//Menor 9 < 10 true
+//Maior 10 > 11 false
+//Menor 11 < 10 false
+//! = Negacao/Aversao
+//
+//
+var batata88 = 88
+console.log("Batata\n", 
+"Batata1\n", batata88)
+
+
+
+
+
+
+
+
+
+
+
